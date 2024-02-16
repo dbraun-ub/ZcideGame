c8bcbd35bd1037706091914803684bb3fb5c16d4
dbraun-ub <daniel.braun.ub@gmail.com>
2024-02-15 21:22:10 -0500

    update gameMap to handle class Zone. New test cases for both Zone and GameMap classes

--- src/rules/gameMap.py
+++ src/rules/gameMap.py
@@ -1,46 +1,67 @@
 ## In Zombicide, a game map is composed of zones. Each zone can be separated by walls, doors or open path. actors can move through zones. 
 ## This class is objective is to handle the connexion between zones and how they are connected.
 
+from .zone import Zone
+
 class GameMap:
     def __init__(self, connections={}):
-        self.zonesConnections = connections
+        self.zones = []
 
-    def writeConnections(self, connections):
-        # It will erase previous connections
-        for key in connections.keys():
-            self.zonesConnections[key] = connections[key]
+    def addZone(self, newZone: Zone):
+        self.zones.append(newZone)
 
-    def resetConnections(self):
-        self.zonesConnections = {}
+    def connectZones(self, zone1: Zone, zone2: Zone):
+        zone1.addConnection(zone2)
+        zone2.addConnection(zone1)
+
+    def connectZonesWithId(self, id1, id2):
+        id1Exists = False
+        id2Exists = False
+        for z in self.zones:
+            if z.id == id1: 
+                id1Exists = True
+                zone1 = z
+            if z.id == id2: 
+                id2Exists = True
+                zone2 = z
+            if id2Exists and id1Exists: 
+                break
+
+        if not id1Exists: 
+            zone1 = Zone(id1)
+            self.addZone(zone1)
+        if not id2Exists: 
+            zone2 = Zone(id2)
+            self.addZone(zone2)
+
+        self.connectZones(zone1, zone2)
 
     # Shortest path from a to b. 
     # Return the distance and each valid solution. 
     # If no valide solution, return distance -1 and empty list.
-    def shortestPath(self, a: int, b: int):
-        if (a not in self.zonesConnections) or (b not in self.zonesConnections):
+    def shortestPathZones(self, zone1: Zone, zone2: Zone):
+        if (zone1 not in self.zones) or (zone2 not in self.zones):
             return [[]]
 
         # Distance one between each zone.
-        N = len(self.zonesConnections)
-        visitedZones = [a]
+        N = len(self.zones)
+        visitedZones = [zone1]
         memoPath = [[]]*N
-        memoPath[0] = [[a]]
+        memoPath[0] = [[zone1]]
 
         # Loop at most until every zone has been explored. It can be shorter.
         i = 0
-        while (b not in visitedZones) and (i < N):
+        while (zone2 not in visitedZones) and (i < N):
             memoPath[i+1] = []
             for p in memoPath[i]:
                 # look for the next destination
-                for neighbor in self.zonesConnections[p[-1]]:
+                for neighbor in p[-1].connectedZones:
                     if neighbor in p:
                         continue
                     memoPath[i+1] += [p + [neighbor]]
                     visitedZones.append(neighbor)
             i += 1
 
-            
-        outputPath = [validPath for validPath in memoPath[i] if b in validPath]
+        outputPath = [validPath for validPath in memoPath[i] if zone2 in validPath]
         return outputPath
-
 
--- src/tests/gameMapTests.py
+++ src/tests/gameMapTests.py
@@ -3,45 +3,47 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from rules import GameMap
+from rules import GameMap, Zone
 
-# Create simple connections
-def simpleCase_writeConnections():
-    simpleMap = {0:[1,2], 1:[0,3], 2:[0,3], 3:[1,2]}
-
+def simpleCase_createZones():
     myMap = GameMap()
-    myMap.writeConnections(simpleMap)
-    assert(myMap.zonesConnections == simpleMap)
+    zone1 = Zone(1)
+    zone2 = Zone(2)
 
-    myMap2 = GameMap(simpleMap)
-    assert(myMap.zonesConnections == simpleMap)
+    myMap.addZone(zone1)
+    myMap.addZone(zone2)
+    myMap.connectZones(zone1, zone2)
 
-    print("simpleCase_writeConnections: OK")
+    assert(zone1.connectedZones == myMap.zones[0].connectedZones)
+    assert(zone2.connectedZones == myMap.zones[1].connectedZones)
 
-# reset the gamemap
-def simpleCase_resetConnections():
+    print("simpleCase_createZones: OK")
+
+def simpleCase_findPathZones(trueShortestPath: list, connections: map, startPosition: int, endPosition: int):
     myMap = GameMap()
-    simpleMap = {0:(1,2), 1:(0,3), 2:(0,3), 3:(1,2)}
-    myMap.writeConnections(simpleMap)
-    myMap.resetConnections()
-    assert(myMap.zonesConnections == {})
-    print("simpleCase_resetConnections: OK")
 
-def simpleCase_findPath(trueShortestPath: list, connections: map, startPosition: int, endPosition: int):
-    myMap = GameMap(connections)
-    shortestPath = myMap.shortestPath(startPosition,endPosition)
+    for k in connections.keys():
+        for c in connections[k]:
+            myMap.connectZonesWithId(k, c)
+
+    for z in myMap.zones:
+        if startPosition == z.id:
+            zone1 = z
+        if endPosition == z.id:
+            zone2 = z
+
+    shortestPath = myMap.shortestPathZones(zone1, zone2)
+
     for i in range(len(trueShortestPath)):
         for j in range(len(trueShortestPath[i])):
-            assert(shortestPath[i][j] == trueShortestPath[i][j])
+            assert(shortestPath[i][j].id == trueShortestPath[i][j])
 
-    print("simpleCase_findPath: OK")
-
+    print("simpleCase_findPathZones: OK")
 
 
 
 if __name__ == "__main__":
-    simpleCase_writeConnections()
-    simpleCase_resetConnections()
-    simpleCase_findPath([[0,1]], {0:[1], 1:[0]}, 0, 1)
-    simpleCase_findPath([[0,3,4,7]], {0:[1,2,3], 1:[0,3], 2:[0,5], 3:[0,1,4], 4:[3,7], 5:[2,6], 6:[5,7], 7:[4,6]}, 0, 7)
-    simpleCase_findPath([[1,2,4,8,7],[1,3,4,8,7]], {0:[1], 1:[0,2,3], 2:[1,4], 3:[1,4], 4:[2,3,8], 5:[6], 6:[5,7], 7:[6,8], 8:[4,7,9], 9:[8]}, 1, 7)
+    simpleCase_createZones()
+    simpleCase_findPathZones([[0,1]], {0:[1], 1:[0]}, 0, 1)
+    simpleCase_findPathZones([[0,3,4,7]], {0:[1,2,3], 1:[0,3], 2:[0,5], 3:[0,1,4], 4:[3,7], 5:[2,6], 6:[5,7], 7:[4,6]}, 0, 7)
+    simpleCase_findPathZones([[1,2,4,8,7],[1,3,4,8,7]], {0:[1], 1:[0,2,3], 2:[1,4], 3:[1,4], 4:[2,3,8], 5:[6], 6:[5,7], 7:[6,8], 8:[4,7,9], 9:[8]}, 1, 7)
